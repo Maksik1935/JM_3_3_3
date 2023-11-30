c8dc18dce4611ea853207328647660f9af16278a
m-kac <m-kachalov@mail.ru>
2023-11-30 15:37:57 +0300

    Initial commit

---
+++ src/main/java/com/example/jm_3_3_3/Jm333Application.java
@@ -0,0 +1,13 @@
+package com.example.jm_3_3_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jm333Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jm333Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jm_3_3_3/Jm333ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jm_3_3_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jm333ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
